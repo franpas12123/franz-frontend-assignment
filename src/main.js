58b93528313fb991a59ad406b524fec258632669
Frances Pasulohan <franpas12123@gmail.com>
2021-11-17 02:58:34 +0800

    add fontawesome icons

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faKey, faUserPlus, faAt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-Vue.config.productionTip = false
+library.add(faUser, faKey, faUserPlus, faAt);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
