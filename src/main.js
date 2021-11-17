9443f7a6cb959e8b8074f2a7e74a66fef0604427
Frances Pasulohan <franpas12123@gmail.com>
2021-11-18 04:40:14 +0800

    add new icons

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faKey, faUserPlus, faAt } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faKey, faUserEdit, faAt,faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Gravatar from 'vue-gravatar';
 import './assets/css/main.css';
 
-library.add(faUser, faKey, faUserPlus, faAt);
+library.add(faUser, faKey, faUserEdit, faAt,faCheck, faTimes);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.component('v-gravatar', Gravatar);
