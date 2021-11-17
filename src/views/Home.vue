435089fcea01c6d185c9cb955bb97966302a1b24
Frances Pasulohan <franpas12123@gmail.com>
2021-11-18 04:42:08 +0800

    add spacer and gallery

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,6 +29,31 @@
         </v-btn>
       </div>
     </v-container>
+    <v-divider class="mt-3"></v-divider>
+    <v-container>
+      <h1>Gallery</h1>
+      <v-row>
+        <v-col v-for="n in 9" :key="n" class="d-flex child-flex" cols="4">
+          <v-card elevation="5">
+            <v-img
+              :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+              :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+              aspect-ratio="1"
+              class="grey lighten-2"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
