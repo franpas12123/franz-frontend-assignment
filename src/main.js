b7b3a153edd024314e51274dcbe7e2eb71fc68a3
Frances Pasulohan <franpas12123@gmail.com>
2021-11-17 15:19:10 +0800

    add gravatar

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import vuetify from './plugins/vuetify';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUser, faKey, faUserPlus, faAt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import Gravatar from 'vue-gravatar';
 
 library.add(faUser, faKey, faUserPlus, faAt);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
+Vue.component('v-gravatar', Gravatar);
 
 Vue.config.productionTip = false;
 
