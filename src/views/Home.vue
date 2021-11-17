c234e6bdb1125edfcb4e5d5b223aa23c8fb90969
Frances Pasulohan <franpas12123@gmail.com>
2021-11-18 04:41:49 +0800

    add user info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,52 @@
 <template>
   <div>
-    <h1>Lorem ipsum dolor</h1>
-    <h2>Lorem ipsum dolor</h2>
-    <h3>Lorem ipsum dolor</h3>
-    <h4>Lorem ipsum dolor</h4>
-    <h5>Lorem ipsum dolor</h5>
-    <h6>Lorem ipsum dolor</h6>
+    <v-img src="../assets/bg.jpg" max-height="150"></v-img>
+    <v-container>
+      <div class="gravatar-container mt-n16 d-flex">
+        <v-gravatar
+          :class="`rounded-xl ${$vuetify.breakpoint.xs ? 'mx-auto' : ''}`"
+          :email="email"
+          :size="150"
+          default-img="mm"
+        />
+        <div class="my-auto" v-if="$vuetify.breakpoint.smAndUp">
+          <div class="mx-5 mt-10">
+            <h1>
+              <strong>{{ firstName }} {{ lastName }}</strong>
+            </h1>
+            <h4 class="font-weight-light">{{ email }}</h4>
+          </div>
+        </div>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          class="custom-btn mt-16"
+          @click="$router.push({ name: 'EditAccount' })"
+          fab
+          depressed
+        >
+          <font-awesome-icon class="icon" icon="user-edit" />
+        </v-btn>
+      </div>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  created() {
+    const { firstName, lastName, email } = this.$store.getters.profile;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  },
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+    };
+  },
 };
 </script>
